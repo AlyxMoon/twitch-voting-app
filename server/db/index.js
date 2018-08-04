ecc90585617525e011bebc26c13a6574dc7a9c4a
Allister <allisterkmoon@gmail.com>
2018-08-04 11:51:20 -0700

    Implement all basic REST methods for polls

--- server/db/index.js
+++ server/db/index.js
@@ -14,14 +14,63 @@ module.exports = {
       console.log('connection to rethinkDB success')
     })
   },
+
+  get: ({ model = '', id = 0 }) => {
+    return new Promise((resolve, reject) => {
+      if (!isKnownModel(model)) {
+        return reject(new Error(`Model ${model} was not recognized as a valid type`))
+      }
+
+      return resolve(knownModels[model].get(id).getJoin().run())
+    })
+  },
+
+  find: ({ model = '' }) => {
+    return new Promise((resolve, reject) => {
+      if (!isKnownModel(model)) {
+        return reject(new Error(`Model ${model} was not recognized as a valid type`))
+      }
+
+      return resolve(knownModels[model].run())
+    })
+  },
+
   create: ({ model = '', data = {} }) => {
     return new Promise((resolve, reject) => {
-      if (!Object.keys(knownModels).some(knownModel => knownModel === model)) {
+      if (!isKnownModel(model)) {
         return reject(new Error(`Model ${model} was not recognized as a valid type`))
       }
 
       const newModel = new knownModels[model](data)
       return resolve(newModel.saveAll())
     })
+  },
+
+  update: ({ model = '', id, data = {} }) => {
+    return new Promise((resolve, reject) => {
+      if (!isKnownModel(model)) {
+        return reject(new Error(`Model ${model} was not recognized as a valid type`))
+      }
+
+      knownModels[model].get(id).getJoin().run().then(row => {
+        resolve(row.merge(data).saveAll())
+      })
+    })
+  },
+
+  delete: ({ model = '', id }) => {
+    return new Promise((resolve, reject) => {
+      if (!isKnownModel(model)) {
+        return reject(new Error(`Model ${model} was not recognized as a valid type`))
+      }
+
+      knownModels[model].get(id).getJoin().run().then(row => {
+        resolve(row.deleteAll())
+      })
+    })
   }
+}
+
+const isKnownModel = (model) => {
+  return Object.keys(knownModels).some(knownModel => knownModel === model)
 }
--- server/routes/polls.js
+++ server/routes/polls.js
@@ -1,39 +1,90 @@
 const path = require('path')
 
 const routes = require('express').Router()
-const { create } = require(path.join(__dirname, '..', 'db'))
+const db = require(path.join(__dirname, '..', 'db'))
 
+const model = { model: 'Poll' }
+
+// FIND ALL
 routes.get('/', (req, res) => {
-  // TODO @allistermoon: Implement find all
-})
-
-routes.get('/:id', (req, res) => {
-  // TODO @allistermoon: Implement find single
-})
-
-routes.delete('/:id', (req, res) => {
-  // TODO @allistermoon: Implement delete
-})
-
-// CREATE
-routes.post('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
-  let { model, data } = req.body
-  if (!model || !data) {
-    return res.json({ success: false, error: 'a required field was not included' })
-  }
-  if (data.id) delete data.id
 
-  create({ model, data })
-    .then(res.json)
+  db.find({ ...model })
+    .then(result => {
+      res.json({ success: false, data: result })
+    })
     .catch(error => {
       console.error(error.message, error.stack)
       res.json({ success: false, error: error.message })
     })
 })
 
+// GET
+routes.get('/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  let id = req.params.id
+
+  db.get({ ...model, id })
+    .then(result => {
+      res.json({ success: true, data: result })
+    })
+    .catch(error => {
+      console.error(error.message, error.stack)
+      res.json({ success: false, error: error.message })
+    })
+})
+
+// CREATE
+routes.post('/', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  let { data } = req.body
+  if (!data) {
+    return res.json({ success: false, error: 'a required field was not included' })
+  }
+  if (data.id) delete data.id
+
+  db.create({ ...model, data })
+    .then(result => {
+      res.json({ success: true, data: result })
+    })
+    .catch(error => {
+      console.error(error.message, error.stack)
+      res.json({ success: false, error: error.message })
+    })
+})
+
+// UPDATE
 routes.post('/:id', (req, res) => {
-  // TODO @allistermoon: Implement update
+  res.setHeader('Content-Type', 'application/json')
+  let id = req.params.id
+  let { data } = req.body
+  if (!data) {
+    return res.json({ success: false, error: 'a required field was not included' })
+  }
+  if (data.id) delete data.id
+
+  db.update({ ...model, id, data })
+    .then(result => {
+      res.json({ success: true, data: result })
+    })
+    .catch(error => {
+      console.error(error.message, error.stack)
+      res.json({ success: false, error: error.message })
+    })
+})
+
+// DELETE
+routes.delete('/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  let id = req.params.id
+
+  db.delete({ ...model, id })
+    .then(result => {
+      res.json({ success: true, data: result })
+    })
+    .catch(error => {
+      res.json({ success: false, error: error.message })
+    })
 })
 
 module.exports = routes
