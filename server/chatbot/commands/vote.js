852fc85291f60a2ecd9d001a64d7d3b60f4cff43
Allister <allisterkmoon@gmail.com>
2018-08-27 15:29:28 -0700

    Correct typo for display-name in bot vote

--- server/chatbot/commands/vote.js
+++ server/chatbot/commands/vote.js
@@ -35,7 +35,7 @@ const vote = ({ context, params, bot }) => {
     .then(poll => {
       if (!poll) return
 
-      let query = `?twitchId=${context.user['user-id']}&displayname=${context.user['display_name']}&username=${context.user.username}`
+      let query = `?twitchId=${context.user['user-id']}&displayname=${context.user['display-name']}&username=${context.user.username}`
 
       return fetch(`http://localhost:8080/api/polls/${poll.id}/votes/add/${game.guid}${query}`)
         .then(response => response.json())
